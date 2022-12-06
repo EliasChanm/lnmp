1a96e85137a5e680d1f7c6f0bc71ceecb6f41c71
Elias steve <110704667+EliasChanm@users.noreply.github.com>
2022-12-06 17:41:01 +0800

    Update database.sh

--- database.sh
+++ database.sh
@@ -8,10 +8,6 @@ systemctl enable mariadb --now
 # configure mariadb
 mysql -u root <<'EOF'
 UPDATE mysql.user SET password=password('AWSsourceEnv2022') WHERE user='root';
-flush privileges;
-CREATE USER 'wordpress-user' IDENTIFIED BY 'AWSRocksSince2006';
-CREATE DATABASE `wordpress-db`;
-GRANT ALL PRIVILEGES ON `wordpress-db`.* TO "wordpress-user";
 FLUSH PRIVILEGES;
 EOF
 
